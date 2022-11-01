06573256d923c3966760845166d9f7093cf3ecad
Louis <louis.morgan@hotmail.com>
2022-11-01 12:18:41 +0000

    fix refresh route

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,31 @@
+/* eslint-disable no-restricted-globals */
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from './App';
 import './custom.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="projects/footystats">
-    <App />
-  </BrowserRouter>,
-);
+const history = createBrowserHistory();
+
+const app = document.getElementById('root');
+if (app) {
+  // 1. Set up the browser history with the updated location
+  // (minus the # sign)
+  const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+  if (path) {
+    history.replace(path);
+  }
+
+  console.log(path);
+
+  // 2. Render our app
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <BrowserRouter basename="projects/footystats">
+      <App />
+    </BrowserRouter>,
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
